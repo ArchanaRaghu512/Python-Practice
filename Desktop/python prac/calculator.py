c95f6cf2763a52542f1664d20bc88687b45eded6
Archana Raghu <araghu@unomaha.edu>
2017-05-28 21:23:29 +0530

    basic calculator

---
+++ Desktop/python prac/calculator.py
@@ -0,0 +1,23 @@
+class calculator:
+
+    def addition(x,y):
+        added = x + y
+        print(added)
+
+    def subtraction(x,y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x,y):
+        product = x * y
+        print(product)
+
+    def division(x,y):
+        div = x / y
+        print(div)
+
+calculator.addition(2,3)
+calculator.subtraction(5,1)
+calculator.multiplication(3,10)
+calculator.division(34,3)
+

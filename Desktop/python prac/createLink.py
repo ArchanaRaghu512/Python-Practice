c99f42545ee208da8ce94cc0fe811fc20e835373
ArchanaRaghu512 <archana.iyer1991@gmail.com>
2017-06-13 01:42:37 -0500

    Create createLink.py

---
+++ Desktop/python prac/createLink.py
@@ -0,0 +1,18 @@
+'''
+program : create links
+Author: Archana
+
+'''
+
+heroes = ['batman', 'superman', 'wonderwoman', 'aquaman']
+for i in heroes:
+    print(i)
+
+url = 'www.heroes.com'
+for i in heroes:
+    print(url+'/'+i+'.txt')
+
+urll = []
+for i in heroes:
+    links = url+'/'+'.txt'
+    urll.append(links)

ea201e0e63ec5b8753f4837de119ae76e08c5751
ArchanaRaghu512 <archana.iyer1991@gmail.com>
2017-06-15 23:59:58 -0500

    Update ListManipulation.py

--- Desktop/python prac/ListManipulation.py
+++ Desktop/python prac/ListManipulation.py
@@ -16,3 +16,17 @@ x.remove(2)
 #will print the values from index (5) to (9)
 print(x[5:9])
 
+#will print the third element from the last print(x[-y])
+print(x[-3])
+
+#will print the index of the number index(x)
+print(x.index(2))
+
+#will automatically sort the values of list in ascending order x.sort()
+#where x = the list
+x.sort()
+
+
+
+
+

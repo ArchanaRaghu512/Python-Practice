cb8a1654dbb153ba9585bbe6cc5429f99b3621c2
ArchanaRaghu512 <archana.iyer1991@gmail.com>
2017-06-15 23:56:57 -0500

    Create ListManipulation.py

---
+++ Desktop/python prac/ListManipulation.py
@@ -0,0 +1,18 @@
+'''
+Program : List operations
+Author : Archaha
+'''
+
+#x = [1,2,3,4,5,6,7,8]
+x.append(2)
+print(x)
+
+#Insert operation (position, value to be inserted)
+x.insert(1,34)
+
+#remove function deletes the first available value (x) in the list
+x.remove(2)
+
+#will print the values from index (5) to (9)
+print(x[5:9])
+
